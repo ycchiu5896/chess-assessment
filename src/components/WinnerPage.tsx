4c758a6a2112487100355a61e6adec9f77f5569a
Yi Chung Chiu <ycchiu5896@gmail.com>
2025-02-07 11:48:12 -0800

    add WinnerPage logic, display winner and when reset the game when click button

--- src/components/WinnerPage.tsx
+++ src/components/WinnerPage.tsx
@@ -1,7 +1,10 @@
 export default function WinnerPage({winner, onStateChange} : {winner: 'white' | 'black' | null, onStateChange: (winner: 'white' | 'black' | null) => void}) {
     return (
         <div>
-            WinnerPage
+            <div className={`winner-page ${winner}-wins`}>
+                <h1>{winner} wins</h1>
+            </div>
+            <button onClick={() => onStateChange(null)}> Play Again</button>
         </div>
     )
 }
